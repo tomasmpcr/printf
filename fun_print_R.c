884163dc16295dce7c9f567ef4e3f4c15654d5d7
Federico Blengio <fedeeblengio@gmail.com>
2021-03-16 19:10:30 +0000

    betty style fun_R.c

--- fun_print_R.c
+++ fun_print_R.c
@@ -10,38 +10,45 @@
 int fun_print_R(__attribute__((unused)) char *flags, va_list ap)
 {
 	char m[53] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-        char n[53] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-        int i, j;
-        char *s;
+	char n[53] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, j;
+	char *s;
 
-        s = va_arg(ap, char*);
-        if (!s)
-                return (-1);
-        for (i = 0; s[i] != 0; i++)
-        {
-                for (j = 0; m[j] != '\0'; j++)
-                {
-                        if (s[i] == m[j])
-                        {
-                                _putchar(n[j]);
-                                break;
-                        }
-                }
-                if (!m[j])
-                        _putchar(s[i]);
-        }
-        return (_strlen(s));
+	s = va_arg(ap, char*);
+	if (!s)
+		return (-1);
+	for (i = 0; s[i] != 0; i++)
+	{
+		for (j = 0; m[j] != '\0'; j++)
+		{
+			if (s[i] == m[j])
+			{
+				_putchar(n[j]);
+				break;
+			}
+		}
+		if (!m[j])
+			_putchar(s[i]);
+		}
+	return (_strlen(s));
 }
+
+/**
+* _strlen - function of strlen
+* @s: pointer
+* ------------------------------------
+* Return: return the characters
+*/
 
 int _strlen(char *s)
 {
-        int len = 0;
+	int len = 0;
 
-        while (*s != 0)
-        {
-                s++;
-                len++;
-        }
-        return (len);
+	while (*s != 0)
+	{
+		s++;
+		len++;
+	}
+	return (len);
 }
 
