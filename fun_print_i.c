0f83663e7e8e77cd16282096e20a021ec1825898
tshuna332 <tmmi333gmail.com>
2021-03-16 14:11:50 +0000

    other solution for the last commit

--- fun_print_i.c
+++ fun_print_i.c
@@ -1,55 +1,34 @@
 #include "holberton.h"
 
 /**
- * print_number - loco..
- *@n: num
- *
- * Return: void.
- */
-void print_number(int n)
-{
-	unsigned int num;
-
-	if (n < 0)
-	{
-		_putchar('-');
-		num = -n;
-	}
-	else
-		num = n;
-
-	if ((num / 10) != 0)
-		print_number(num / 10);
-
-	_putchar((num % 10) + '0');
-}
-
-/**
 * fun_print_i -function of integer
 * @ap: parametrs of numbers
 * @flags: the flags
 * ------------------------------------
 * Return: return the characters
 */
-
-int fun_print_i(__attribute__((unused)) char *flags, va_list ap)
-{
-	int n, len = 0, div = 1, i;
-	n = va_arg(ap, int);
-
-	print_number(n);
-
-	if (n < 0)
+int fun_print_i(__attribute__((unused)) char *flags, va_list ap) {
+	int i, len = 0, div = 1;
+	unsigned int num;
+	i = va_arg(ap, int);
+	if (i < 0)
 	{
-		n = n * -1;
+		num = i * -1;
+		_putchar('-');
 		len++;
 	}
-
-	for (i = 0; (n / div) > 10; i++)
+	else
+		num = i;
+	for (i = 0; (num / div) > 10; i++)
 	{
 		div = div * 10;
 	}
-	len += i + 1;
 
+	for (; (i + 1) > 0; i--)
+	{
+		_putchar(((num / div) % 10) + '0');
+		div = div / 10;
+		len++;
+	}
 	return (len);
 }
