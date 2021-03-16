7d37ecb3be7c535b9726eae5d36698dacb049d66
Federico Blengio <fedeeblengio@gmail.com>
2021-03-16 18:29:45 +0000

    fun.R default

--- fun_print_R.c
+++ fun_print_R.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
-* fun_print_R -function of character
+* fun_print_R - function of rot13
 * @ap: parametrs of numbers
 * @flags: the flags
 * ------------------------------------
@@ -9,38 +9,11 @@
 
 int fun_print_R(__attribute__((unused)) char *flags, va_list ap)
 {
-	char m[53] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-        char n[53] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-        int i, j;
-        char *s;
+	char c;
 
-        s = va_arg(ap, char*);
-        if (!s)
-                return (-1);
-        for (i = 0; s[i] != 0; i++)
-        {
-                for (j = 0; m[j] != '\0'; j++)
-                {
-                        if (s[i] == m[j])
-                        {
-                                _putchar(n[j]);
-                                break;
-                        }
-                }
-                if (!m[j])
-                        _putchar(s[i]);
-        }
-        return (_strlen(s));
+	c = va_arg(ap, int);
+	_putchar(c);
+
+	return (1);
 }
 
-int _strlen(char *s)
-{
-        int len = 0;
-
-        while (*s != 0)
-        {
-                s++;
-                len++;
-        }
-        return (len);
-}
