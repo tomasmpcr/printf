3ededa23ea97eef611b8792501235b182fbfb4f8
tshuna332 <tmmi333gmail.com>
2021-03-16 13:25:05 +0000

    fix function not use malloc

--- fun_print_i.c
+++ fun_print_i.c
@@ -10,43 +10,28 @@
 
 int fun_print_i(__attribute__((unused)) char *flags, va_list ap)
 {
-	int aux, i, len, num, neg = 0;
-	char *listNuambers;
+	int i, len, num, neg = 0, div = 1;
 
 	num = va_arg(ap, int);
 
-	if  (num == 0)
-	{
-		_putchar('0');
-		return (1);
-	}
 	if (num < 0)
 	{
 		neg = 1;
 		num = num * -1;
 		_putchar('-');
 	}
-	aux = num;
-	for (i = 0; aux != 0 ; i++)
+
+	for (i = 0; (num / div) > 10; i++)
 	{
-		aux = aux / 10;
+		div = div * 10;
 	}
-	len = i;
-	listNuambers = malloc((i + 1) * sizeof(char));
-	if (listNuambers == NULL)
+	len = i + 1;
+
+	for (; (i + 1) > 0; i--)
 	{
-		return (-1);
+		_putchar(((num / div) % 10) + '0');
+		div = div / 10;
 	}
-	aux = num;
-	for (i = 0 ; aux != 0 ; i++)
-	{
-		listNuambers[i] = (aux % 10) + 48;
-		aux = aux / 10;
-	}
-	listNuambers[i] = '\0';
-	for (i = (len - 1) ; i >= 0 ; i--)
-	{
-		_putchar(listNuambers[i]);
-	}
+
 	return (len + neg);
 }
